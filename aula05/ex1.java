a14600a8cff17bbb704d12d2f137409a9f601a4a
Jeferson1709 <jefersonalvessantos62@gmail.com>
2025-04-06 18:50:40 -0300

    initial

---
+++ aula05/ex1.java
@@ -0,0 +1,11 @@
+package aula05;
+
+// Faça uma programa que imprima a seguinte sequência:
+// 100,99,98...1
+public class ex1 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i--) {
+            System.out.println("Números: " + i);
+        }
+    }
+}
