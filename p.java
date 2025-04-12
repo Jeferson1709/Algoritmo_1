4625d9894ad9ed83faee8aade5e13400d0f37634
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-04-12 13:09:47 -0300

    Delete p.java

--- p.java
+++
@@ -1,8 +0,0 @@
-public class p {
-    public static void main(String[] args) {
-        int num = 10;
-        int num2 = 20;
-        String o = (num2 > 18) ? "Good day" : "Good evenving";
-        System.out.println(o);
-    }
-}
