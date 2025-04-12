7abd69991a5a543748e68c2fc7feed65b7874690
Jeferson Alves <jefersonalvessantos62@gmail.com>
2025-04-12 13:09:16 -0300

    Delete pra.java

--- pra.java
+++
@@ -1,8 +0,0 @@
-public class pra {
-    public static void main(String[] args) {
-        int time = 20;
-        String result;
-        result = (time < 18) ? "Good day." : "Good evening.";
-        System.out.println(result);
-    }
-}
